e7e6d5ecfe645bb1e4cb4766ab357f099baf0306
Henrique <henrique.ribeiro@hardworkmedicina.com.br>
2024-08-13 13:41:41 -0300

    mocka o retorno da api

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,26 @@ import Floresta from "@/public/assets/img/floresta.jpg";
 import Divider from "@/components/divider/divider";
 import CardPost from "@/components/cardpost/cardpost";
 
+//Esse bloco será alterado posteriormente para o retorno da API
+const response = [
+  {
+    img: Menino,
+    img_descricao: "menino lendo",
+    titulo: "A Terra onde os sonhos se tornam realidade",
+    data_post: "21-07-2024",
+    descricao:
+      "Se os seus sonhos não derem certo, Estarei sempre lá para você. Se precisar se esconder, Terá sempre minha mão. Mesmo se o céu desabar, Estarei sempre contigo.",
+  },
+  {
+    img: Floresta,
+    img_descricao: "foto da floresta",
+    titulo: "Viva, aproveite, registre!",
+    data_post: "26-07-2024",
+    descricao:
+      "Aproveitar a vida pra mim? Tomar impulso ao caminho do céu. Harmonizar-me com meu irmão. Silenciar e estar em paz comigo. Aproveitar a vida depende do que você ...",
+  },
+];
+
 export default function Home() {
   return (
     <div
@@ -21,22 +41,16 @@ export default function Home() {
         <h1 className={clsx("text-4xl", "text-neutral-500")}>Todos os posts</h1>
       </div>
       <Divider />
-      <CardPost
-        img={Menino}
-        alt="menino lendo"
-        title="A Terra onde os sonhos se tornam realidade"
-        dataPost="21-07-2024"
-        description="Se os seus sonhos não derem certo, Estarei sempre lá para você. Se precisar se esconder, Terá sempre minha mão. Mesmo se o céu desabar, Estarei sempre contigo."
-      />
-
-      <Divider />
-      <CardPost
-        img={Floresta}
-        alt="foto da floresta"
-        title="Viva, aproveite, registre!"
-        dataPost="26-07-2024"
-        description="Aproveitar a vida pra mim? Tomar impulso ao caminho do céu. Harmonizar-me com meu irmão. Silenciar e estar em paz comigo. Aproveitar a vida depende do que você ..."
-      />
+      {response.map((e, index) => (
+        <CardPost
+          key={index}
+          img={e.img}
+          alt={e.img_descricao}
+          title={e.titulo}
+          dataPost={e.data_post}
+          description={e.descricao}
+        />
+      ))}
     </div>
   );
 }
