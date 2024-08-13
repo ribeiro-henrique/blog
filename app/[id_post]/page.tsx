818239354f12216fa14c4e53fc3c23596f35de20
Henrique <henrique.ribeiro@hardworkmedicina.com.br>
2024-08-13 14:32:13 -0300

    cria estrutura da pagina do post

--- app/[id_post]/page.tsx
+++ app/[id_post]/page.tsx
@@ -1,12 +1,28 @@
 "use client";
-import { useParams } from "next/navigation";
+
+//React imports
+import { useParams, useRouter } from "next/navigation";
+import Image from "next/image";
+
+//Libs
+import clsx from "clsx";
+
+//Assets
+import Menino from "@/public/assets/img/menino.jpg";
 
 export default function Post() {
   const { id_post } = useParams();
+  const router = useRouter();
 
   return (
-    <div>
-      <h1>{id_post}</h1>
-    </div>
+    <main className={clsx("w-full px-6 py-8")}>
+      <nav>
+        <button onClick={() => router.back()}>Voltar</button>
+      </nav>
+      <div>
+        <Image src={Menino} alt="" />
+        <h1>{id_post}</h1>
+      </div>
+    </main>
   );
 }
