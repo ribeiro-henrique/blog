1ec90f78b0d04169651dae60c12415a305d5f7bb
Henrique <hesr.ribeiro@gmail.com>
2024-08-13 21:44:00 -0300

    componentiza o post page

--- app/[id_post]/page.tsx
+++ app/[id_post]/page.tsx
@@ -2,21 +2,39 @@
 
 //React imports
 import { useParams, useRouter } from "next/navigation";
-import Image from "next/image";
 
 //Libs
 import clsx from "clsx";
 
 //Components
-import Divider from "@/components/divider/divider";
 import Navigation from "@/components/navigation/navigation";
+import ContentPost from "@/components/contentpost/contentpost";
 
 //Assets
 import Menino from "@/public/assets/img/menino.jpg";
+import { useEffect, useState } from "react";
 
 export default function Post() {
   const { id_post } = useParams();
+
   const router = useRouter();
+
+  const [post, setPost] = useState<any>();
+
+  const idPost = {
+    imagem: Menino,
+    alt: "Menino",
+    titulo: "A Terra onde os sonhos se tornam realidade",
+    data: "13/08/2024",
+    conteudo:
+      "A vida é um turbilhão. Uma dança frenética entre alegrias efêmeras e tristezas profundas, entre a certeza inabalável e a dúvida que nos assola no meio da noite. Em meio a esse caos, buscamos âncoras, faróis que iluminem o caminho e nos guiem em meio à névoa densa da incerteza. Para muitos, como eu, essas âncoras residem na fé, na família e na busca incessante por significado em um mundo que muitas vezes parece desprovido de sentido. E a família? Aquela constelação de almas que, por laços de sangue ou por laços do coração, se unem para trilhar juntas a jornada da vida. Um porto seguro em meio à tempestade, um abraço acolhedor após um longo dia. É na família que aprendemos o significado do amor incondicional, da paciência, do perdão. É no seio familiar que construímos nossas primeiras memórias, que aprendemos a lidar com as diferenças, a celebrar as conquistas e a oferecer o ombro amigo nos momentos de dor.",
+  };
+
+  //Valor mockado, vou pegar da API posteriormente
+  //Dinamicamente, com base no id do post
+  useEffect(() => {
+    setPost(idPost);
+  }, []);
 
   return (
     <main
@@ -26,43 +44,14 @@ export default function Post() {
       )}
     >
       <Navigation handleClick={() => router.back()} label="Voltar" />
-      <div
-        className={clsx("flex flex-col w-full", "items-center justify-center")}
-      >
-        <h1 className={clsx("text-4xl", "text-neutral-500 text-center")}>
-          A Terra onde os sonhos se tornam realidade
-        </h1>
-        <Divider />
 
-        <span className={clsx("text-zinc-400", "italic")}>13/08/2024</span>
-
-        <Image src={Menino} alt="" />
-
-        <div className={clsx("w-full py-6")}>
-          <p className={clsx("text-zinc-500", "text-center")}>
-            A vida é um turbilhão. Uma dança frenética entre alegrias efêmeras e
-            tristezas profundas, entre a certeza inabalável e a dúvida que nos
-            assola no meio da noite. Em meio a esse caos, buscamos âncoras,
-            faróis que iluminem o caminho e nos guiem em meio à névoa densa da
-            incerteza. Para muitos, como eu, essas âncoras residem na fé, na
-            família e na busca incessante por significado em um mundo que muitas
-            vezes parece desprovido de sentido.
-            <br />
-            <br />E a família? Aquela constelação de almas que, por laços de
-            sangue ou por laços do coração, se unem para trilhar juntas a
-            jornada da vida. Um porto seguro em meio à tempestade, um abraço
-            acolhedor após um longo dia. É na família que aprendemos o
-            significado do amor incondicional, da paciência, do perdão. É no
-            seio familiar que construímos nossas primeiras memórias, que
-            aprendemos a lidar com as diferenças, a celebrar as conquistas e a
-            oferecer o ombro amigo nos momentos de dor.
-          </p>
-        </div>
-        <div className={clsx("w-full flex flex-col", "italic")}>
-          <span className={clsx("text-zinc-500")}>Autor:</span>
-          <span className={clsx("text-zinc-500")}>Pr Ronaldo Ribeiro</span>
-        </div>
-      </div>
+      <ContentPost
+        img={post?.imagem}
+        alt={post?.alt}
+        title={post?.titulo}
+        date={post?.data}
+        content={post?.conteudo}
+      />
     </main>
   );
 }
